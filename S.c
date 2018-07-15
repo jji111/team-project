17c88deae4b1d1ba172378364770b603b2b5577d
Lansional <37794353+Lansional@users.noreply.github.com>
2018-07-15 21:59:12 +0900

    Update S.c

--- S.c
+++ S.c
@@ -1,94 +1,65 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <Windows.h>
+#include <stdio.h>		// 표준 C라이브러리
+#include <stdlib.h>	
+#include <string.h>		// strcpy, atoi, memset
+#include <Windows.h>	// 소켓, pause
 #include <process.h>
 
-#pragma comment(lib, "ws2_32.lib") 
+#pragma comment(lib, "ws2_32.lib")	// 자동 라이브러리 넣기
 
-#define BUF_SIZE 100 // 채팅 내용 사이즈
-#define MAX_CLNT 4 // 클라이언트 max
-#define _CRT_SECURE_NO_WARNINGS
+#define BUF_SIZE 100
+#define MAX_CLNT 4		// 최대 사용자(클라이언트)
 
-unsigned WINAPI HandleClnt(void *arg); // 값 받기
-void SendMsg(char *msg, int len); //보내기
-void ErrorHandling(char *msg); //error 출력
+unsigned WINAPI HandleClnt(void *arg);
+void SendMsg(char *msg, int len);	// 클라이언트에서 
+void ErrorHandling(char *msg);
 
-int clntCnt = 0; //클라이언트 갯수 새기
+// 매개 변수
+int clntCnt = 0;
 SOCKET clntSocks[MAX_CLNT];
 HANDLE hMutex;
 
-char player1;
-char player2;
-char player3;
-char player4;
-int pl = 0;
-int maxpl = 0;
-
 int main(int argc, char *argv[]) {
-	while (1)
-	{
-		printf("인원 수를 입력해주세요 : ");
-		gets(&pl);
-		maxpl = (int)pl - 48;
-		if (maxpl < 2) {
-			printf("최소 2명을 입력해주세요!\n");
-		}
-		else if (maxpl > 4) {
-			printf("최대 4명입니다!\n\n");
-		}
-		else{
-			break;
-		}
-	}
 	WSADATA wsaData;
 	SOCKET hServSock, hClntSock;
 	SOCKADDR_IN servAdr, clntAdr;
 	int clntAdrSz;
 	HANDLE hThread;
 	printf("port를 입력해주세요 : ");
-	char port[100] = { 0, };
-	int strLen = 0, i;
-	char msg[BUF_SIZE];
-	gets(port);
+	char port[100] = { 0 };
+	fgets(port, 100, stdin);
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		ErrorHandling("WSAStartup() Error");
+		ErrorHandling((char*)"WSAStartup() Error");
 	}
 
 	hMutex = CreateMutex(NULL, FALSE, NULL);
+	// CreateMutex: 원도우 중복 방지실행
 	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-
-	memset(&servAdr, 0, sizeof(servAdr));
+	memset(&servAdr, 0, sizeof(servAdr));	// servAdr의 NULL값으로 초기화
 	servAdr.sin_family = AF_INET;
 	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servAdr.sin_port = htons(atoi(port));
+	// htons: 네트워크 바이트 정렬 방식의 4바이트 데이터를 호스트 바이트 정렬 방식으로 변환
 
 	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
-		ErrorHandling("bind() error");
+		ErrorHandling((char*)"bind() error");
 	}
 	if (listen(hServSock, 5) == SOCKET_ERROR) {
-		ErrorHandling("listen() Error");
+		ErrorHandling((char*)"listen() Error");
 	}
 
+	// 소켓 신호 받기
 	while (1) {
 		clntAdrSz = sizeof(clntAdr);
 		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &clntAdrSz);
-
+		puts("클라이언트에서 메세지 받는 중: ");
+		if (-1 == hClntSock)
+			ErrorHandling((char*)"클라이언트 연결 수락 실패!");
 		WaitForSingleObject(hMutex, INFINITE);
 		clntSocks[clntCnt++] = hClntSock;
-		ReleaseMutex(hMutex);
+		ReleaseMutex(hMutex);	// 윈도우 중복방지 제거
 		hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void*)&hClntSock, 0, NULL);
-		printf("Connect client IP : %s \n", inet_ntoa(clntAdr.sin_addr));
-		SendMsg(maxpl , 1);
-
-		if (clntCnt > maxpl) {
-			printf("최대 인원은 %d 명이 입니다. 사용자가 너무 많아 서버를 종료합니다.", maxpl);
-			closesocket(hServSock);
-			WSACleanup();
-			system("pause");
-			return 0;
-		}
+		printf("Connect client IP : %s\n", inet_ntoa(clntAdr.sin_addr));	// inet_ntoa: 네트워크 주소 반환
 	}
 
 	closesocket(hServSock);
@@ -100,28 +71,24 @@ int main(int argc, char *argv[]) {
 unsigned WINAPI HandleClnt(void *arg) {
 	SOCKET hClntSock = *((SOCKET*)arg);
 	int strLen = 0, i;
-	while (1) {
-		char msg[BUF_SIZE] = { 0, };
-		if ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0) {
-			if (strLen == -1) {
-				system("pause");
-				return 0;
-			}
-			SendMsg(msg, strLen);
-			printf("%s\n",msg);
-		}
+	char msg[BUF_SIZE];
+
+	while ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0) {
+		SendMsg(msg, strLen);
+		fputs(msg, stdout);
 	}
 
 	WaitForSingleObject(hMutex, INFINITE);
+
 	for (i = 0; i < clntCnt; i++) {
 		if (hClntSock == clntSocks[i]) {
-			while (i++ < clntCnt - 1) {
+			while (i++ < clntCnt - 1)
 				clntSocks[i] = clntSocks[i + 1];
-			}
 			break;
 		}
 	}
 	clntCnt--;
+
 	ReleaseMutex(hMutex);
 	closesocket(hClntSock);
 	return 0;
@@ -136,6 +103,7 @@ void SendMsg(char *msg, int len) {
 	ReleaseMutex(hMutex);
 }
 
+// 에러
 void ErrorHandling(char *msg) {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
